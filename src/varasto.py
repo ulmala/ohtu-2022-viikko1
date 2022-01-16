e4ed374602cfe0babf5acfe4e116564edc454573
Olli Ulmala <ulmala.olli@hotmail.com>
2022-01-16 13:12:40 +0200

    fix code

--- src/varasto.py
+++ src/varasto.py
@@ -42,4 +42,4 @@ class Varasto:
         return maara
     
     def __str__(self):
-        return f"saldo X = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
